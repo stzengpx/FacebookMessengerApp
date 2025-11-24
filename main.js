2d2eda33f9ef8e74adbaea7edb05fef0e9a5e1e8
Heresy <kheresy@gmail.com>
2025-11-24 14:07:19 +0800

    make it package as installer
    - allow to pin in taskbar
    - open link in system browser

--- main.js
+++ main.js
@@ -1,20 +1,46 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, shell } = require('electron');
 const path = require('path');
+const windowStateKeeper = require('electron-window-state');
+
+if (process.platform === 'win32' && !app.isPackaged) {
+  app.setAppUserModelId('com.electron.fbmessenger');
+}
 
 function createWindow() {
+  // Load the previous state with fallback to defaults
+  let mainWindowState = windowStateKeeper({
+    defaultWidth: 640,
+    defaultHeight: 800
+  });
+
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 640,
-    height: 800,
+    x: mainWindowState.x,
+    y: mainWindowState.y,
+    width: mainWindowState.width,
+    height: mainWindowState.height,
     webPreferences: {
       nodeIntegration: false,
       contextIsolation: true,
     }
   });
 
+  // Let us register listeners on the window, so we can update the state
+  // automatically (the listeners will be removed when the window is closed)
+  // and restore the maximized or full screen state
+  mainWindowState.manage(mainWindow);
+
   // Load the Facebook Messages URL.
   mainWindow.loadURL('https://www.messenger.com/');
 
+  // Open links externally
+  mainWindow.webContents.setWindowOpenHandler(({ url }) => {
+    if (!url.includes('messenger.com')) {
+      shell.openExternal(url);
+      return { action: 'deny' };
+    }
+    return { action: 'allow' };
+  });
 }
 
 // This method will be called when Electron has finished
